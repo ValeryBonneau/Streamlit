9be05a21cc9244df2644b68caeafe191ca57e4a7
ValeryBonneau <valery.bonneau@gmail.com>
2023-09-21 10:27:53 +0200

    draft first app

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import pickle
-
-st.write("""
-# Test App for Omdena
-I will make some look and feel test and enhancement for an Omdena project
-         """)
